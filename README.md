908a7839ba502ec213e867b3680f8f174fb962da
acesnik <acesnik@chem.wisc.edu>
2018-05-14 18:37:06 -0500

    readme and test

---
+++ README.md
@@ -0,0 +1,4 @@
+# Proteogenomics
+C# implementation of classes and functions for proteogenomics
+
+[![Build status](https://ci.appveyor.com/api/projects/status/t5l1ikqbihfhaui5?svg=true)](https://ci.appveyor.com/project/smith-chem-wisc/proteogenomics)
---
+++ TestProteogenomics/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestProteogenomics
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void test()
+        {
+
+        }
+    }
+}

b62c09d080de15e67fab378b6334432e01cfab79
acesnik <acesnik@chem.wisc.edu>
2018-05-19 20:23:22 -0500

    Fix benchmark program

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -1,37 +1,72 @@
-﻿using System;
-using Bio;
+﻿using Proteogenomics;
 using Proteomics;
-using Proteogenomics;
+using System;
 using System.Collections.Generic;
-using UsefulProteomicsDatabases;
-using System.Linq;
 using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using UsefulProteomicsDatabases;
 
 namespace Benchmark
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SameProteins();
         }
 
-        public void SameProteins()
+        private static void SameProteins()
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            // download and decompress references
-            EnsemblDownloadsWrapper downloads = new EnsemblDownloadsWrapper();
-            downloads.DownloadReferences(
-                TestContext.CurrentContext.TestDirectory,
-                Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData"),
-                "grch38");
-            ProteinAnnotation.GetImportantProteinAccessions(TestContext.CurrentContext.TestDirectory, Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", EnsemblDownloadsWrapper.GRCh38ProteinFastaFilename));
 
-            Genome genome = new Genome(downloads.GenomeFastaPath);
-            GeneModel geneModel = new GeneModel(genome, downloads.Gff3GeneModelPath);
-            List<Protein> geneBasedProteins = geneModel.Translate(true, downloads.BadProteinAccessions, downloads.SelenocysteineProteinAccessions);
-            List<Protein> pepDotAll = ProteinDbLoader.LoadProteinFasta(downloads.ProteinFastaPath, true, DecoyType.None, false,
+            // download and decompress references
+            using (WebClient Client = new WebClient())
+            {
+                Client.DownloadFile(@"ftp://ftp.ensembl.org/pub/release-81//fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz", "Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz");
+                Client.DownloadFile(@"ftp://ftp.ensembl.org/pub/release-81/gff3/homo_sapiens/Homo_sapiens.GRCh38.81.gff3.gz", "Homo_sapiens.GRCh38.81.gff3.gz");
+                Client.DownloadFile(@"ftp://ftp.ensembl.org/pub/release-81//fasta/homo_sapiens/pep/Homo_sapiens.GRCh38.pep.all.fa.gz", "Homo_sapiens.GRCh38.pep.all.fa.gz");
+            }
+            string genomeFasta = "Homo_sapiens.GRCh38.dna.primary_assembly.fa";
+            string geneModelFile = "Homo_sapiens.GRCh38.81.gff3";
+            string proteinFasta = "Homo_sapiens.GRCh38.pep.all.fa";
+            if (!File.Exists(genomeFasta))
+            {
+                using (FileStream stream = new FileStream("Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz", FileMode.Open))
+                using (GZipStream gunzip = new GZipStream(stream, CompressionMode.Decompress))
+                using (var f = File.Create(proteinFasta))
+                {
+                    gunzip.CopyTo(f);
+                }
+            }
+            if (!File.Exists(geneModelFile))
+            {
+                using (FileStream stream = new FileStream("Homo_sapiens.GRCh38.81.gff3.gz", FileMode.Open))
+                using (GZipStream gunzip = new GZipStream(stream, CompressionMode.Decompress))
+                using (var f = File.Create(proteinFasta))
+                {
+                    gunzip.CopyTo(f);
+                }
+            }
+            if (!File.Exists(proteinFasta))
+            {
+                using (FileStream stream = new FileStream("Homo_sapiens.GRCh38.pep.all.fa.gz", FileMode.Open))
+                using (GZipStream gunzip = new GZipStream(stream, CompressionMode.Decompress))
+                using (var f = File.Create(proteinFasta))
+                {
+                    gunzip.CopyTo(f);
+                }
+            }
+
+            ProteinAnnotation.GetImportantProteinAccessions(proteinFasta, out Dictionary<string, string> proteinAccessionSequence, out HashSet<string> bad, out Dictionary<string, string> se);
+
+            Genome genome = new Genome(genomeFasta);
+            GeneModel geneModel = new GeneModel(genome, geneModelFile);
+            List<Protein> geneBasedProteins = geneModel.Translate(true, bad, se);
+            List<Protein> pepDotAll = ProteinDbLoader.LoadProteinFasta(proteinFasta, true, DecoyType.None, false,
                 ProteinDbLoader.EnsemblAccessionRegex, ProteinDbLoader.EnsemblFullNameRegex, ProteinDbLoader.EnsemblFullNameRegex, ProteinDbLoader.EnsemblGeneNameRegex, null, out List<string> errors);
             Dictionary<string, string> accSeq = geneBasedProteins.ToDictionary(p => p.Accession, p => p.BaseSequence);
             stopwatch.Stop();
@@ -50,9 +85,25 @@ namespace Benchmark
                 }
             }
 
+            stopwatch.Stop();
             Console.WriteLine("Finished checking that all proteins are the same.");
-            Console.WriteLine("Time elapsed: " + stopwatch.Elapsed.TotalHours.ToString() + ":" + stopwatch.Elapsed.Minutes.ToString() + ":" + stopwatch.Elapsed.Seconds.ToString());
-            Console.WriteLine("Result: all proteins are " + (allAreEqual ? "" : "not") + " equal ");
+            Console.WriteLine("Time elapsed: "+ stopwatch.Elapsed.Minutes.ToString() + " minutes and " + stopwatch.Elapsed.Seconds.ToString() + " seconds.");
+            Console.WriteLine("Result: all proteins are " + (allAreEqual ? "" : "not ") + "equal ");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            foreach (var file in Directory.GetFiles(Environment.CurrentDirectory, Path.GetFileNameWithoutExtension(genomeFasta) + "*"))
+            {
+                File.Delete(file);
+            }
+            foreach (var file in Directory.GetFiles(Environment.CurrentDirectory, Path.GetFileNameWithoutExtension(geneModelFile) + "*"))
+            {
+                File.Delete(file);
+            }
+            foreach (var file in Directory.GetFiles(Environment.CurrentDirectory, Path.GetFileNameWithoutExtension(proteinFasta) + "*"))
+            {
+                File.Delete(file);
+            }
         }
     }
 }

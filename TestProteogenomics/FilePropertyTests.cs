5a98a972f2bae5f9d93487a27f7ead672c70a656
acesnik <acesnik@chem.wisc.edu>
2018-06-07 18:35:06 -0500

    Brought in BAMProperties

---
+++ TestProteogenomics/FilePropertyTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Proteogenomics;
+using System.IO;
+using System;
+
+namespace TestProteogenomics
+{
+    [TestFixture]
+    public class FilePropertyTests
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            (new GeneModelTests()).Setup();
+        }
+
+        [Test]
+        public void CountReads()
+        {
+            var fastq = new FastqProperties(Path.Combine(TestContext.CurrentContext.TestDirectory, "TestFastqs", "mapper.fastq"));
+            Assert.AreEqual(3970, fastq.ReadCount);
+            Assert.AreEqual(65.3, Math.Round(fastq.AverageReadLength, 1));
+        }
+
+        [Test]
+        public void BAMPropertiesStrandSpecificityTest()
+        {
+            BAMProperties bam = new BAMProperties(
+                Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", "unstrandedSingle202122.bam"),
+                Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", "202122.gtf"),
+                new Genome(Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", "202122.fa")),
+                0.8);
+            Assert.AreEqual(Strandedness.None, bam.Strandedness);
+            Assert.AreEqual(RnaSeqProtocol.SingleEnd, bam.Protocol);
+        }
+    }
+}
